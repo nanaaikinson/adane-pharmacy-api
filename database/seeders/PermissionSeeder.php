bef952d02446a6954baefddd0fb16024e38f374e
Nana Kwesi Ofosu-Aikins <61139144+nanaaikinson@users.noreply.github.com>
2020-12-03 08:31:39 +0000

    ProductController Stock method added

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,17 +2,56 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
 
 class PermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // Roles
+    Permission::insert([
+      [
+        "name" => "create-role", "display_name" => "create role",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ],
+      [
+        "name" => "read-role", "display_name" => "view role",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ],
+      [
+        "name" => "update-role", "display_name" => "update role",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ],
+      [
+        "name" => "delete-role", "display_name" => "delete role",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ]
+    ]);
+
+    // Users
+    Permission::insert([
+      [
+        "name" => "create-user", "display_name" => "create user",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ],
+      [
+        "name" => "read-user", "display_name" => "view user",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ],
+      [
+        "name" => "update-user", "display_name" => "update user",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ],
+      [
+        "name" => "delete-user", "display_name" => "delete user",
+        "created_at" => gmdate("Y-m-d H:i:s"), "updated_at" => gmdate("Y-m-d H:i:s")
+      ]
+    ]);
+  }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Functions\Mask;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
@@ -13,6 +15,22 @@ class RoleSeeder extends Seeder
    */
   public function run()
   {
+    Role::create([
+      "name" => "developer",
+      "display-name" => "Developer",
+      "mask" => Mask::integer()
+    ]);
 
+    Role::create([
+      "name" => "admin",
+      "display-name" => "Admin",
+      "mask" => Mask::integer()
+    ]);
+
+    Role::create([
+      "name" => "dispenser",
+      "display-name" => "Dispenser",
+      "mask" => Mask::integer()
+    ]);
   }
 }
