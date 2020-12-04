e1f3c8190e4c7fd783d21fef8ac7d73c1d50f984
Nana Kwesi Ofosu-Aikins <nanaaikinson2020@gmail.com>
2020-12-04 09:25:01 +0000

    Request facade changed to http

--- app/Http/Controllers/API/Admin/AuthController.php
+++ app/Http/Controllers/API/Admin/AuthController.php
@@ -8,7 +8,7 @@ use App\Traits\ResponseTrait;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
