0a068883792667435f5aa774c33df26600587701
Nana Aikinson <nanaaikinson2020@gmail.com>
2021-02-05 23:51:42 +0000

    Notices

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -16,39 +16,24 @@ class Customer extends Authenticatable implements Auditable
   use \OwenIt\Auditing\Auditable;
   use HasApiTokens;
 
-  /**
-   * The attributes that are guarded.
-   *
-   * @var array
-   */
   protected $guarded = [];
 
-  /**
-   * The attributes that should be hidden for arrays.
-   *
-   * @var array
-   */
   protected $hidden = [
     'password',
     'remember_token',
   ];
 
-  /**
-   * The attributes that should be cast to native types.
-   *
-   * @var array
-   */
   protected $casts = [
     'email_verified_at' => 'datetime',
   ];
 
-  /**
-   * Get full name of user
-   *
-   * @return string
-   */
   public function getNameAttribute(): string
   {
     return "{$this->first_name} {$this->last_name}";
+  }
+
+  public function dewormingNotices()
+  {
+    return $this->hasMany(CustomerDewormingNotice::class);
   }
 }
