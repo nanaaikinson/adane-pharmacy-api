2820cd54b67f31949cc960b823b16c912b5bfb2f
Nana Aikinson <nanaaikinson2020@gmail.com>
2021-05-28 15:45:17 +0000

    Update of StorePurchaseRequest

--- app/Http/Requests/StorePurchaseRequest.php
+++ app/Http/Requests/StorePurchaseRequest.php
@@ -28,13 +28,13 @@ class StorePurchaseRequest extends FormRequest
       "purchase_date" => "required|date|date_format:Y-m-d",
       "invoice_number" => "required",
       "details" => "nullable",
-//      "items" => "required|array",
-//      "items.*.product_id" => "required|exists:products,id",
-//      "items.*.has_expiry" => "required",
-//      "items.*.expiry_date" => "required_if:has_expiry,1",
-//      "items.*.quantity" => "required|numeric|min:1",
-//      "items.*.cost_price" => "required|regex:/^\d+(\.\d{1,2})?$/",
-//      "items.*.selling_price" => "required|regex:/^\d+(\.\d{1,2})?$/",
+      "items" => "required|array",
+      "items.*.product_id" => "required|exists:products,id",
+      "items.*.has_expiry" => "required",
+      "items.*.expiry_date" => "required_if:has_expiry,1",
+      "items.*.quantity" => "required|numeric|min:1",
+      "items.*.cost_price" => "required|regex:/^\d+(\.\d{1,2})?$/",
+      "items.*.selling_price" => "required|regex:/^\d+(\.\d{1,2})?$/",
     ];
   }
 
